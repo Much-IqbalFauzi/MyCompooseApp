b7bc6d09a1ee380ed59425006e5b904980b0e8f1
Much. Iqbal Fauzi <672018382@student.uksw.edu>
2022-07-03 14:22:40 +0700

    List

--- app/src/main/java/com/example/myapps/MainActivity.kt
+++ app/src/main/java/com/example/myapps/MainActivity.kt
@@ -3,22 +3,104 @@ package com.example.myapps
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapps.ui.theme.MyAppsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO Template
-
         setContent {
-
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    LazyColumn{
+                        items(count = 5000) { idx ->
+                            Text(
+                                text = "Item $idx",
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(12.dp)
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .background(Color.Cyan)
+                            .fillMaxSize()
+                            .weight(1f),
+                    ) {
+                        val scrollState = rememberScrollState()
+                        Column(
+                            modifier = Modifier.verticalScroll(scrollState)
+                        ) {
+                            for (i in 1..50) {
+                                Text(
+                                    text = "Item $i",
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 24.dp)
+                                )
+                            }
+                        }
+                    }
+                    Box(modifier = Modifier
+                        .background(Color.Magenta)
+                        .fillMaxSize()
+                        .weight(1f)
+                    ) {
+                        LazyColumn {
+                            itemsIndexed(
+                                "Hello world, my name's Iqbal Fauzi, currently learning jetpact compose".split(" ")
+                            ) { index, item ->  
+                                Text(
+                                    text = "$index. $item",
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 24.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
