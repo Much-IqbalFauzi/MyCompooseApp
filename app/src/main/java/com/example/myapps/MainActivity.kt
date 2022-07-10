e987f8f2ff798f69ca8101131066b8c17aa87b7f
Much. Iqbal Fauzi <672018382@student.uksw.edu>
2022-07-10 22:07:17 +0700

    bottom sheet

--- app/src/main/java/com/example/myapps/MainActivity.kt
+++ app/src/main/java/com/example/myapps/MainActivity.kt
@@ -3,21 +3,79 @@ package com.example.myapps
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapps.ui.theme.MyAppsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO Template
 
         setContent {
+            val sheetState = rememberBottomSheetState(
+                initialValue = BottomSheetValue.Collapsed
+            )
+            val scaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = sheetState
+            )
+            val coroutineScope = rememberCoroutineScope()
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                sheetContent = {
+                    BtmSheetContent(coroutineScope, sheetState)
+                },
+                sheetBackgroundColor = Color.Magenta
+            ) {
+                ScaffoldContent()
+            }
+        }
+    }
+
+    @Composable
+    fun ScaffoldContent() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(text = "Scaffold Content", fontSize = 64.sp)
+
+        }
+    }
+
+    @Composable
+    fun BtmSheetContent(coroutineScope: CoroutineScope, sheetState: BottomSheetState) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(Color.Green)
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    if (sheetState.isCollapsed) {
+                        sheetState.expand()
+                    } else {
+                        sheetState.collapse()
+                    }
+                }
+            }) {
+                Text(text = "Toggle")
+            }
 
         }
     }
